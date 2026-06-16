c46951c57d36f63012354c28712df279bab2543d
spoorjith1 <spoorjith1@gmail.com>
2026-06-16 17:04:55 +0530

    frontend started

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <footer className='footer-box'>
+        <div className='footer-top'>
+          <p className='footer-copyright'>Go Bus &copy; 2026</p>
+          <p className='made-by'>Made by <span className='made-by-name'>Spoorjith Malebalu</span></p>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
