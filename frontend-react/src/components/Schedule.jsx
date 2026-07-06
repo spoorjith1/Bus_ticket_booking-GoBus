1ed0c54f1e169ce279f87e9b8d66ad6612152d8a
spoorjith1 <spoorjith1@gmail.com>
2026-07-06 18:45:28 +0530

    search buses

--- frontend-react/src/components/Schedule.jsx
+++ frontend-react/src/components/Schedule.jsx
@@ -1,9 +1,56 @@
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import '../styles/SearchBuses.css'
 
 function Schedule({ schedule }) {
   return (
-    <div>
-      sss
+    <div className='bsc-sch-box'>
+      <div className='bsc-sch-in'>
+        <div>
+          <h3 className='bsc-sch-op-name'>{schedule.bus_operator}</h3>
+          <p><span className='bsc-sch-span'>Bus Name : </span>{schedule.bus_name}</p>
+          <p><span className='bsc-sch-span'>Bus Number : </span>{schedule.bus_number}</p>
+          <p className='bsc-bus-type'><span className='bsc-sch-span'>Bus Type : </span><span className='bsc-sch-bus-type'>{schedule.bus_type}</span></p>
+        </div>
+
+        <div className='bsc-sch-route-info'>
+          <div className='bsc-route-info-in'>
+            <p className='bsc-sch-route'>{schedule.source}</p>
+            <p className='bsc-sch-datetime'>{new Date(schedule.departure_datetime).toLocaleString()}</p>
+          </div>
+          <div className='bsc-route-info-in bsc-route-in-mid'>
+            <FontAwesomeIcon icon={faArrowRight} className='bsc-sch-right-arrow' />
+            <div>
+              <p className='bsc-duration'>{schedule.total_duration}</p>
+              <p className='bsc-distance'>{schedule.distance}Km</p>
+            </div>
+          </div>
+          <div className='bsc-route-info-in'>
+            <p className='bsc-sch-route'>{schedule.destination}</p>
+            <p className='bsc-sch-datetime'>{new Date(schedule.arrival_datetime).toLocaleString()}</p>
+          </div>
+        </div>
+
+        <div className='bsc-in-sec-last'>
+          <p className='bsc-sec-last-date'>{new Date(schedule.departure_datetime).toLocaleDateString('en-IN', {
+            weekday: 'short',
+            day: 'numeric',
+            month: 'numeric',
+            year: 'numeric',
+            timeZone: 'UTC'
+          })}</p>
+          <div>
+            <p className='bsc-fare'>₹{schedule.fare}</p>
+            <p className='bsc-per-seat'>Per Person</p>
+          </div>
+        </div>
+
+        <div className='bsc-sch-last'>
+          <button className='bsc-book-now-btn'>Book Now</button>
+          <p className='bsc-seats'>{schedule.available_seats} seats available</p>
+        </div>
+      </div>
     </div>
   )
 }
--- frontend-react/src/pages/SearchBuses.jsx
+++ frontend-react/src/pages/SearchBuses.jsx
@@ -1,9 +1,113 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightArrowLeft, faCalendarDays, faL, faLocationDot, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import '../styles/SearchBuses.css'
+import axiosInstance from '../axiosInstance'
+import Schedule from '../components/Schedule'
 
 function SearchBuses() {
+  const [source, setSource] = useState('')
+  const [destination, setDestination] = useState('')
+  const [journeyDate, setJourneyDate] = useState('')
+  const [schedules, setSchdeules] = useState([])
+  const [schedulesError, setSchedulesError] = useState('')
+  const [schedulesLoading, setSchedulesLoading] = useState(false)
+
+  const today = new Date().toISOString().split('T')[0]
+  const maxDate = new Date()
+  maxDate.setDate(maxDate.getDate() + 15)
+  const lastDate = maxDate.toISOString().split('T')[0]
+
+  const reverseRoute = ()=> {
+    setSource(destination)
+    setDestination(source)
+  }
+
+  const fetchSchedules = async ()=> {
+    setSchedulesLoading(true)
+    try {
+      const response = await axiosInstance.get('/schedule/list/')
+      setSchdeules(response.data)
+    }
+    catch (error) {
+      setSchdeulesError('Failed to load schedules')
+    }
+    finally {
+      setSchedulesLoading(false)
+    }
+  }
+
+  const handleSearch = async () => {
+    try {
+      const response = await axiosInstance.get('/schedules/search/', {
+        params: {
+          'source': source,
+          'destination': destination,
+          'journey_date': journeyDate
+        },
+      })
+      setSchdeules(response.data)
+    }
+    catch (error) {
+      setSchedulesError('Failed to ')
+    }
+  }
+
+  useEffect(()=> {
+    fetchSchedules();
+  }, [])
+
   return (
-    <div className='page-container'>
-      Search Buses
+    <div className='page-container search-buses-page'>
+      <div className='bsbar-floating-container'>
+        <div className='bsbar-box'>
+
+          <div className='bsbar-route-box'>
+            <div className='bsbar-input-box'>
+              <FontAwesomeIcon icon={faLocationDot} className='bsbar-input-icons' />
+              <div className='bsbar-input-box-in'>
+                <label className='bsbar-input-labels'>From</label>
+                <input type='text' className='bsbar-input-fields' value={source} onChange={(e)=> setSource(e.target.value)} />
+              </div>
+            </div>
+
+            <button className='bsbar-arrows-btn' onClick={()=> reverseRoute()}><FontAwesomeIcon icon={faArrowRightArrowLeft} /></button>
+
+            <div className='bsbar-input-box'>
+              <FontAwesomeIcon icon={faLocationDot} className='bsbar-input-icons' />
+              <div className='bsbar-input-box-in'>
+                <label className='bsbar-input-labels'>To</label>
+                <input type='text' className='bsbar-input-fields' value={destination} onChange={(e)=> setDestination(e.target.value)} />
+              </div>
+            </div>
+          </div>
+          
+          <div className='bsbar-input-box'>
+            <FontAwesomeIcon icon={faCalendarDays} className='bsbar-input-icons' />
+            <div className='bsbar-input-box-in'>
+              <label className='bsbar-input-labels'>Date</label>
+              <input type='date' className='bsbar-input-fields bsbar-input-date' min={today} max={lastDate} 
+              value={journeyDate} onChange={(e)=> setJourneyDate(e.target.value)} />
+            </div>
+          </div>
+          
+          <button className='bsbar-search-btn' onClick={handleSearch}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} className='bsbar-search-icon' /> Search Buses
+          </button>
+        </div>
+      </div>
+
+      <div>
+        {schedules.length === 0 ? (
+          <div className='bs-no-buses-msg'>"No buses available."</div>
+        ) : (
+          <div className='bs-grid-container'>
+            {schedules.map((schedule)=> (
+              <Schedule key={schedule.id} schedule={schedule} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
