ecdf0d4e0c514c7e32777aa0de83b0401606d147
spoorjith1 <spoorjith1@gmail.com>
2026-07-07 21:12:44 +0530

    urls

--- backend-drf/api/urls.py
+++ backend-drf/api/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     path('operator/buses/<int:id>/update/', BusViews.BusUpdateView.as_view(), name='update_bus'), #op
     path('operator/buses/<int:id>/delete/', BusViews.BusDeleteView.as_view(), name='delete_bus'), #op
     path('operator/buses/list/', BusViews.BusListView.as_view(), name='bus_list'), #op ✅
-    path('operator/buses/<int:id>/detail/', BusViews.BusDetailView.as_view(), name='bus_detail'), #op
+    path('operator/buses/<int:id>/detail/', BusViews.BusDetailView.as_view(), name='bus_detail'), #op ✅
     
     #Routes
     path('routes/create/', RouteViews.RouteCreateView.as_view(), name='create_route'), #admin ✅
@@ -53,13 +53,13 @@ urlpatterns = [
     path('operator/schedules/list/', ScheduleViews.OperatorSchedulesListView.as_view(), name='list_operators_schedule'), #op ✅
     path('operator/schedules/<int:id>/details/', ScheduleViews.OperatorSchedulesDetailView.as_view(), name='detail_operator_schedule'), #op
     #Open Schedules for all
-    path('schedule/list/', ScheduleViews.ScheduleListView.as_view(), name='list_schedules'), #all
-    path('schedule/<int:id>/details/', ScheduleViews.ScheduleDetailView.as_view(), name='detail_schedule'), #all
+    path('schedule/list/', ScheduleViews.ScheduleListView.as_view(), name='list_schedules'), #all ✅
+    path('schedule/<int:id>/details/', ScheduleViews.ScheduleDetailView.as_view(), name='detail_schedule'), #all ✅
     #Search Schedules
-    path('schedules/search/', ScheduleViews.ScheduleSearchView.as_view(), name='search_schedules'), #all
+    path('schedules/search/', ScheduleViews.ScheduleSearchView.as_view(), name='search_schedules'), #all ✅
     
     #Seats
-    path('schedules/<int:id>/seats/', ScheduleSeatsView.as_view(), name='schedule_seats'), #user
+    path('schedules/<int:id>/seats/', ScheduleSeatsView.as_view(), name='schedule_seats'), #user ✅
     
     #Booking Summary
     path('bookings/summary/', BookingViews.BookingSummaryView.as_view(), name='booking_summary'), #user
