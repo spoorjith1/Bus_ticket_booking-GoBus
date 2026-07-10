2b93570675ea9aefd33df4b5848f67648237d307
spoorjith1 <spoorjith1@gmail.com>
2026-07-10 15:40:46 +0530

    serailizer fields add for boooking

--- backend-drf/bookings/serializers.py
+++ backend-drf/bookings/serializers.py
@@ -7,13 +7,16 @@ class BookingSerializer(serializers.ModelSerializer):
     seat_ids = serializers.ListField(child=serializers.IntegerField(), write_only=True)
     source = serializers.ReadOnlyField(source='schedule.route.source')
     destination = serializers.ReadOnlyField(source='schedule.route.destination')
+    departure_datetime = serializers.ReadOnlyField(source='schedule.departure_datetime')
+    arrival_datetime = serializers.ReadOnlyField(source='schedule.arrival_datetime')
     distance = serializers.ReadOnlyField(source='schedule.route.distance')
+    bus_operator = serializers.ReadOnlyField(source='schedule.bus.operator.op_name')
     bus_name = serializers.ReadOnlyField(source='schedule.bus.bus_name')
     bus_number = serializers.ReadOnlyField(source='schedule.bus.bus_number')
     seat_numbers = serializers.SerializerMethodField()
     class Meta:
         model = Booking
-        fields = ['id' ,'schedule', 'seat_ids', 'bus_name', 'bus_number', 'source', 'destination', 'distance', 'seat_numbers', 'fare_amount', 'tax_percentage', 'tax_amount', 'total_amount', 'booking_status', 'booked_at']
+        fields = ['id' ,'schedule', 'seat_ids', 'bus_name', 'bus_number', 'source', 'destination', 'distance', 'seat_numbers', 'fare_amount', 'tax_percentage', 'tax_amount', 'total_amount', 'booking_status', 'booked_at', 'departure_datetime', 'arrival_datetime', 'bus_operator']
         read_only_fields = ['fare_amount', 'tax_percentage', 'tax_amount', 'total_amount', 'booking_status', 'booked_at']
     
     def get_seat_numbers(self, obj):
